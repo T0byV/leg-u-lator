a4c921eddedf940b6b6cadd0e8d81667e6bb6e9c
Paasman, P.M.N. (Pieter, Student B-EE) <p.m.n.paasman@student.utwente.nl>
2025-04-11 16:34:37 +0200

    Uncomment wifi setup code and bring back hello world

--- InterfaceFirmware/source/main.cpp
+++ InterfaceFirmware/source/main.cpp
@@ -27,20 +27,20 @@ int main()
 
     UART uart_bus{uart0, 16, 17};
     
-    // printf("Connecting to Wi-Fi...\n");
-    // if (cyw43_arch_wifi_connect_timeout_ms("Your Wi-Fi SSID", "Your Wi-Fi Password", CYW43_AUTH_WPA2_AES_PSK, 30000)) {
-    //     printf("failed to connect.\n");
-    //     return 1;
-    // } else {
-    //     printf("Connected.\n");
-    //     // Read the ip address in a human readable way
-    //     uint8_t *ip_address = (uint8_t*)&(cyw43_state.netif[0].ip_addr.addr);
-    //     printf("IP address %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
-    // }
+    printf("Connecting to Wi-Fi...\n");
+    if (cyw43_arch_wifi_connect_timeout_ms("Your Wi-Fi SSID", "Your Wi-Fi Password", CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        printf("failed to connect.\n");
+        return 1;
+    } else {
+        printf("Connected.\n");
+        // Read the ip address in a human readable way
+        uint8_t *ip_address = (uint8_t*)&(cyw43_state.netif[0].ip_addr.addr);
+        printf("IP address %d.%d.%d.%d\n", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
+    }
 
-    // bool s = false;
-    while (true)
-    {
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
         // gpio_put(PICO_DEFAULT_LED_PIN, s);
         // s = !s;
         printf("Cycle start\n");
