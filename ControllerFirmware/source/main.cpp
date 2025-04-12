431ec8fb40a4400e9f466918258ce87e84c8ad6a
Toby <80796996+T0byV@users.noreply.github.com>
2025-04-12 22:22:09 +0200

    Change almost everything

--- ControllerFirmware/source/main.cpp
+++ ControllerFirmware/source/main.cpp
@@ -7,6 +7,7 @@
 #include <drivers/current_sensor.hpp>
 //#include <drivers/fram.hpp>
 #include <drivers/temp_sensor.hpp>
+#include <drivers/heating_element.hpp>
 
 // See: https://www.raspberrypi.com/documentation/pico-sdk/high_level.html#detailed-description-8 for core interaction
 void core1_entry() {
@@ -23,17 +24,6 @@ int main() {
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
     multicore_launch_core1(core1_entry);
-
-    float max_duty_cycle = 0.3;
-    PWM<4> heating_pwm{
-        {{6, 7, 8, 9}}, // GPIO pins used
-        max_duty_cycle
-    };
-
-    // std::array<PWM, 4> heating_pwm_channels = {{
-
-    // }}
-    // EXAMPLE: heating_pwm.set_duty_cycle_safe(6, 40); sets pin 6 to 40% duty cycle
 
     SyncI2CMaster bus0{0, 20, 21, false};
     Sensor<6, 2> sensing{&bus0, {{
@@ -52,38 +42,41 @@ int main() {
         {&bus0, 0x40, 1, 0.1006}  // Channel 4
     }};
 
+    float max_duty_cycle = 0.2;
+
+    std::array<PWM, 4> heating_pwm_channels = {{
+        {6, max_duty_cycle},
+        {7, max_duty_cycle},
+        {8, max_duty_cycle},
+        {9, max_duty_cycle}
+    }};
+
+    float calibration_duty_cycle = 0.3;
+    float max_power_mw_per_element = 2000;
+    std::array<HeatingElement, 4> heating_elements = {{
+        {0, heating_pwm_channels[0], heating_power_sensors[0], max_power_mw_per_element, calibration_duty_cycle},
+        {1, heating_pwm_channels[1], heating_power_sensors[1], max_power_mw_per_element, calibration_duty_cycle},
+        {2, heating_pwm_channels[2], heating_power_sensors[2], max_power_mw_per_element, calibration_duty_cycle},
+        {3, heating_pwm_channels[3], heating_power_sensors[3], max_power_mw_per_element, calibration_duty_cycle}
+    }};
+
     UART uart_bus{uart1, 4, 5};
 
     // bool s = false;
 
-    float actuator_impedances[4] = {};
-    float calibration_duty_cycle = 0.3;
-
-    printf("INFO: Calibrating\n");
-    gpio_put(PICO_DEFAULT_LED_PIN, true);
+    printf("First calibration\n");
     for (int i = 0; i < 4; i++)
     {
-        printf("element %d: ", i);
-
-        heating_pwm.set_duty_cycle_safe((i + 6), calibration_duty_cycle);
-        sleep_ms(750);
-        float bus_voltage_mv = heating_power_sensors[i].read_bus_voltage();
-        float bus_current_iv = heating_power_sensors[i].read_current();
-        float impedance = (bus_voltage_mv * calibration_duty_cycle) / fabs(bus_current_iv);
-        actuator_impedances[i] = impedance;
-        printf("voltage: %.2fmV, current: %.2fmA, impedance: %.2f\n", bus_voltage_mv, bus_current_iv, impedance);
-        heating_pwm.set_duty_cycle_safe((i + 6), 0.0);
+        printf("%.2fohm\t", heating_elements[i].calibrate_impedance());
     }
-    gpio_put(PICO_DEFAULT_LED_PIN, false);
+    printf("\n");
 
 
     float desired_power_mw = 150;
     int calibration_cycle_counter = 0;
-    int calibration_after_cycles = 20;
+    int calibration_after_cycles = 10;
     int cycle_duration_ms = 1000;
 
-    float set_duty_cycle[4] = {0.0, 0.0, 0.0, 0.0};
-    float confirmed_cycle[4] = {0.0, 0.0, 0.0, 0.0};
     while (true)
     {
         // gpio_put(PICO_DEFAULT_LED_PIN, s);
@@ -92,23 +85,11 @@ int main() {
         printf("Cycle start\n");
 
         if (calibration_cycle_counter >= calibration_after_cycles) {
-            printf("INFO: Calibrating\n");
-            gpio_put(PICO_DEFAULT_LED_PIN, true);
-            for (int i = 0; i < 4; i++)
-            {
-                printf("element %d: ", i);
-                float old_duty_cycle = set_duty_cycle[i];
-                heating_pwm.set_duty_cycle_safe((i + 6), calibration_duty_cycle);
-                sleep_ms(750);
-                float bus_voltage_mv = heating_power_sensors[i].read_bus_voltage();
-                float bus_current_iv = heating_power_sensors[i].read_current();
-                float impedance = (bus_voltage_mv * calibration_duty_cycle) / fabs(bus_current_iv);
-                actuator_impedances[i] = impedance;
-                printf("voltage: %.2fmV, current: %.2fmA, impedance: %.2f\n", bus_voltage_mv, bus_current_iv, impedance);
-                heating_pwm.set_duty_cycle_safe((i + 6), old_duty_cycle);
-                sleep_ms(750);
+            printf("Calibrating\n");
+            for (int i = 0; i < 4; i++){
+                printf("%.2fohm\t", heating_elements[i].calibrate_impedance());
             }
-            gpio_put(PICO_DEFAULT_LED_PIN, false);
+            printf("\n");
             calibration_cycle_counter = 0;
         }
         calibration_cycle_counter++;
@@ -116,23 +97,11 @@ int main() {
 
         for (int i = 0; i < 4; i++)
         {
-            float current = (confirmed_cycle[i] * heating_power_sensors[i].read_bus_voltage());
-
-            float measured_power_mw = 0.001 * (current * current) / actuator_impedances[i];
-            float delta_power_mw = desired_power_mw - measured_power_mw;
-
-            // start controller
-            if (delta_power_mw <= 0.0)
-                set_duty_cycle[i] = set_duty_cycle[i] - 0.001;
-            else if (delta_power_mw > 0.0)
-                set_duty_cycle[i] = set_duty_cycle[i] + 0.001;
-            
-            if (set_duty_cycle[i] > max_duty_cycle) set_duty_cycle[i] = max_duty_cycle;
-            if (set_duty_cycle[i] < 0.0) set_duty_cycle[i] = 0.0;
-            // end controller
-            confirmed_cycle[i] = heating_pwm.set_duty_cycle_safe((i + 6), set_duty_cycle[i]);
-            printf("Delta: %.2fmW, MeasPow: %.2fmW, DesiredPow: %.2fmW, current: %.2fmA, NewDutyCycle: %.3f, ConfirmedDutyCycle: %.3f\n", delta_power_mw, measured_power_mw, desired_power_mw, current, set_duty_cycle[i], confirmed_cycle[i]);
+            heating_elements[i].set_power_safe(desired_power_mw);
+            sleep_ms(200);
+            printf("%.2fmW\t", heating_elements[i].get_current_power());
         }
+        printf("\n");
 
         sleep_ms(cycle_duration_ms);
 
